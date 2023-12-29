828c02f36e84b794086de66d6167c9f1daa29249
Justin Gallagher <justin.f.gallagher@gmail.com>
2023-12-29 20:59:23 +0000

    Cleaned up some old files. Updated sdp with bug fixes and pickup logic

--- gym_vrp/envs/sdp2.py
+++ gym_vrp/envs/sdp2.py
@@ -4,27 +4,26 @@ from .irp import IRPEnv
 class SantaIRPEnv(IRPEnv):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.seed = np.random.seed(23)
         self.child_behavior = None
         self.energy = None
-        self.max_energy = 100
-
-        self.seed = np.random.seed(23)
-
+        
         # Configurable reward and penalty values
+        self.max_energy = 100
         self.correct_delivery_reward = 10
         self.incorrect_delivery_penalty = -10
         self.energy_depletion_penalty = -50
 
         # Wind factor related variables
-        self.energy_strategy = "stop" # "stop": Stops the run or "return": Back to depot, apply penalty and continue (default)
+        self.energy_strategy = "return" # "stop": Stops the run or "return": Back to depot, apply penalty and continue (default)
         self.base_energy_consumption_rate = 4
         self.wind_factor_range = (0.8, 1.2)  # Wind can decrease or increase energy consumption
 
         # Santa can carry multiple items
-        self.santa_carrying = {'present': 0, 'coal': 0}
-        self.max_presents = 2
-        self.max_coal = 2
         self.pickup_stragey = "either" # Either: Pickus up either 1 coal or 1 present (defualt), "both": Picks up both upto max, "random": picks up a random between 0 and max for each
+        self.santa_carrying = {'present': 0, 'coal': 0}
+        self.max_presents = 1
+        self.max_coal = 1
         
     def reset(self):
         state = super().reset()
@@ -58,25 +57,24 @@ class SantaIRPEnv(IRPEnv):
                 current_node = self.depots[0]
                 self.energy = self.max_energy
 
-                # Replenish items at the depot based on the pickup strategy
-                if self.pickup_stragey == 'both':
-                    self.santa_carrying = {'present': self.max_presents, 'coal': self.max_coal}
-                elif self.pickup_stragey == 'random':
-                    self.santa_carrying = {'present': np.random.randint(0, self.max_presents + 1), 'coal': np.random.randint(0, self.max_coal + 1)}
-                else:  # 'either'
-                    item_choice = np.random.choice(['present', 'coal'])
-                    self.santa_carrying = {'present': 1, 'coal': 0} if item_choice == "present" else {'present': 0, 'coal': 1}
-
         # Check if the delivery is correct
-        if self.child_behavior[current_node] == 'good' and self.santa_carrying == 'present':
-            reward += self.correct_delivery_reward
-        elif self.child_behavior[current_node] == 'bad' and self.santa_carrying == 'coal':
-            reward += self.correct_delivery_reward
-        else:
-            reward += self.incorrect_delivery_penalty
+        if self.child_behavior[current_node] == 'good':
+            if self.santa_carrying['present'] > 0:
+                # Correct delivery of present
+                reward += self.correct_delivery_reward
+                self.santa_carrying['present'] -= 1  # Update the number of presents Santa is carrying
+            else:
+                # Incorrect delivery (Santa doesn't have a present for a good child)
+                reward += self.incorrect_delivery_penalty
 
-        # Update what Santa is carrying
-        self.santa_carrying = 'present' if self.santa_carrying == 'coal' else 'coal'
+        elif self.child_behavior[current_node] == 'bad':
+            if self.santa_carrying['coal'] > 0:
+                # Correct delivery of coal
+                reward += self.correct_delivery_reward
+                self.santa_carrying['coal'] -= 1  # Update the number of coal Santa is carrying
+            else:
+                # Incorrect delivery (Santa doesn't have coal for a bad child)
+                reward += self.incorrect_delivery_penalty
 
         # Check if Santa is at the depot
         if current_node == self.depots[0]:
